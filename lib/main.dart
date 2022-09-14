5dc1e12268855fee20436a6ee7eaaad6d49ba716
Omar Eldanasoury <omardanasoury8@gmail.com>
2022-09-14 15:17:53 +0300

    changed the return type of the main method to Future<void> instead of void

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
